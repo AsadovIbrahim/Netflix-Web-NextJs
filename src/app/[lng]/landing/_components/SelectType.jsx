23ca0731c3110c0fe498a1e433e5d11a7d6f69c4
AsadovIbrahim <ibrahimasadov31@gmail.com>
2025-02-19 18:26:48 +0400

    last commit

--- src/app/[lng]/landing/_components/SelectType.jsx
+++ src/app/[lng]/landing/_components/SelectType.jsx
@@ -1,7 +1,7 @@
 import { Select, SelectItem } from "@nextui-org/select";
 import { useEffect } from "react";
 
-export const animals = [
+export const selections = [
   { key: "movie", label: "Movies" },
   { key: "tv", label: "Tv Shows" },
 ];
@@ -14,7 +14,7 @@ export default function SelectType({ conentType, setContentType }) {
       disabledKeys={conentType === "movie" ? ["movie"] : ["tv"]}
       placeholder={conentType}
       className="outline-none flex mb-[10px] justify-center items-center border rounded-[4px] border-[#808080B2] w-[200px] h-[46px]"
-      items={animals}
+      items={selections}
       aria-label="Type"
     >
       {(type) => (
